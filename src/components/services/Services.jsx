1b20b6a370a8f94d272e96f4f130dc94f236e0ac
belisky <fiawornunobel@gmail.com>
2022-10-07 08:52:32 +0000

    adds services component

---
+++ src/components/services/Services.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <div>
+        <div className="services-Header">
+            <p>MY SERVICES</p>
+            <h3>What I Do</h3>
+        </div>
+        <div className="services">
+            <div className="service">
+                <div className="service-Logo">
+                    logo
+                </div>
+                <h5 className='service-Header'>Website Design</h5>
+                <p className='service-Body'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae modi, esse iste nisi tenetur sequi?</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Services
