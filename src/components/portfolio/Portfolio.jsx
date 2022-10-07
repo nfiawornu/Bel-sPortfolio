e22d34875fcbc8f6f65ba5fda9eb36e1643d83c8
belisky <fiawornunobel@gmail.com>
2022-10-07 08:52:59 +0000

    adds portfolio component

---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Portfolio.styles.css'
+
+const Portfolio = () => {
+  return (
+    <div>
+        <div className='portfolio-Header'>
+            <p>PORTFOLIO</p>
+            <h3>Recent Works</h3>
+        </div>
+        <div className='portfolios'>
+            portfolio
+        </div>
+        <div className='portfolio-Button'>
+            <p>View All Works </p>
+        </div>
+    </div>
+  )
+}
+
+export default Portfolio
