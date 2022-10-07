a1a632051a36333dd70db0137b6537bde6fce759
belisky <fiawornunobel@gmail.com>
2022-10-07 08:52:12 +0000

    adds a skills component

---
+++ src/components/skills/Skills.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Skills = () => {
+  return (
+    <div> 
+        <div className="skills">
+            <h5>WebDevelopment</h5>
+            <p>React</p>
+            <p>JavaScript</p>
+            <p>Html</p>
+            <p>CSS</p>
+        </div> 
+        <div className="skills">
+            <h5>Devops</h5>
+            <p>React</p>
+            <p>JavaScript</p>
+            <p>Html</p>
+            <p>CSS</p>
+        </div>        
+    </div>
+  )
+}
+
+export default Skills
