ca656cf75a90a207c4c8265b864c97ad62f0276d
Fiawornu Nobel <61013338+nfiawornu@users.noreply.github.com>
2025-07-16 15:16:40 +0000

    Updated Hero.jsx

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -45,7 +45,8 @@ const Hero = () => {
                     </a>
                 </li>
                 <li className='bg-red-900 py-2 px-4 rounded-xl text-white hover:text-red-900 hover:bg-white hover:border-red-900 hover:border-2 hover:font-bold' >
-                    <a href="https://drive.google.com/file/d/1JZaX9FG9kEOQZEbl2n5cmsiWfGf2fZND/view?usp=sharing">
+ 
+                    <a href="https://drive.google.com/file/d/1WweeBSEQuptvzbe6wVCaVaz4gKVZd6s8/view?usp=sharing">
                         Download CV
                     </a>
                 </li>
@@ -55,14 +56,26 @@ const Hero = () => {
             <div className='sm:w-1/2'>
                 <p className='text-2xl font-bold'>Welcome! <b className='text-red-900'>I am</b></p>
                 <h3 className='cursor-pointer text-4xl font-extrabold text-gray-800 hover:scale-125 hover:translate-x-20'> Fiawornu Nobel</h3>
-                <h5 className='text-lg font-medium text-gray-900'> SWE | Devops Engineer | ML enthusiast</h5>
+                <h5 className='text-lg font-medium text-gray-900'> SRE | Devops | SWE | ML </h5>
                 <p className='leading-8 font-light w-1/2 my-5 pr-5 text-lg sm:w-full'>
-                    Devops engineer with a tonne of experience with Devops tools and technologies for building efficient, automated and robust continuous deployment pipelines with an eye for detail to discover pipeline bottlenecks in order to engineer a faster software development lifecycle. </p>
+                  I am a problem-solving-driven DevOps Engineer with deep hands-on experience in modern DevOps tools and cloud-native technologies.
+                  I specialize in building automated, scalable, and resilient CI/CD pipelines that enable faster, safer, and more reliable deployments.
+
+                  With a sharp eye for system performance bottlenecks, I engineer solutions that:
+
+                   ⚡ Accelerate release cycles
+
+                   📈 Enhance infrastructure scalability
+
+                   🔐 Uphold security and reliability best practices
+
+                  Whether it's optimizing deployment workflows or ensuring rock-solid uptime, I bring a balance of speed, precision, and security to every environment I touch.
+                </p>
                 <div >
                     <div>
                         <h4 className='font-medium text-red-800'>FIND ME ON</h4>
                         <div className='flex mb-5 flex-wrap gap-2'>
-                            <a href="https://github.com/belisky/">
+                            <a href="https://github.com/nfiawornu/">
                                 <div className='bg-slate-900 aspect-[1] w-16 h-16 rounded-md mr-5 flex items-center justify-center hover:bg-red-900 hover:shadow-md'>
                                 <BsGithub className='shadow-md text-white text-4xl '/>
                                 </div>
@@ -86,7 +99,7 @@ const Hero = () => {
                     </div>
                     <div className="flex items-center my-5">
                         <div className="bg-slate-900 text-white py-2 px-4 mr-5 hover:bg-red-900 hover:shadow-md">
-                            <h5 className='font-black text-3xl'>2+</h5>
+                            <h5 className='font-black text-3xl'>4+</h5>
                             <p>YEARS OF EXPERIENCE</p>
                         </div>
                         <div className="bg-slate-900 text-white py-2 px-4 hover:bg-red-900 hover:shadow-md">
@@ -96,8 +109,9 @@ const Hero = () => {
                     </div>
                     </div>
             </div>
-            <div className="flex sm:w-1/2 sm:aspect-[16/30] md:aspect-[16/12]">            
-               <img className='object-cover object-center w-full' src="https://drive.google.com/uc?export=view&id=1EQHXCs88QbIoCGXe_PblhQN_ssN7Cdey" alt="my_self" />
+            <div className="flex sm:w-1/2 sm:aspect-[16/30] md:aspect-[16/12]">    
+              
+               <img className='object-cover object-center w-full' src="https://drive.google.com/uc?export=view&id=1Jw-94c11_exttE3PKvhjjWKkJ_klUXCM" alt="my_self" />
             </div>            
         </div>
     </div>
